968982f4b1717a58b7eb00d2c91bb2266d0ddf56
KriPra2024 <kp.cpc2024@gmail.com>
2024-06-12 16:25:09 +0530

    backend done

--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const router=express.Router();
 const {genurl}=require("../controllers/url")
-
+const vo=require("../controllers")
 router.post("/",genurl);
 module.exports=router;
 
