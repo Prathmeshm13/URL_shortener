732c7ba7034ca1256ae409f87b033c78398a5000
KriPra2024 <kp.cpc2024@gmail.com>
2024-06-14 12:25:56 +0530

    jo

--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,5 @@
 const express=require("express");
 const router=express.Router();
-const shortid = require("shortid");
-const URL=require("../models/url")
 const {genurl}=require("../controllers/url")
 const vo=require("../controllers")
 router.post("/",genurl);
