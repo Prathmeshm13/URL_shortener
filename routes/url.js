486c0933f8c54c7a864926355b38111f6a3a93b6
Prathmeshm13 <prathmeshmahakal35@gmail.com>
2024-06-14 12:40:02 +0530

    jfwjf

--- routes/url.js
+++ routes/url.js
@@ -1,5 +1,7 @@
 const express=require("express");
 const router=express.Router();
+const shortid = require("shortid");
+const URL=require("../models/url")
 const {genurl}=require("../controllers/url")
 const vo=require("../controllers")
 router.post("/",genurl);
